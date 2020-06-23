4e8449be7e6dd2d0f212b585fa0c0c18f4e2106a
Unknown <hackurity01@gmail.com>
2020-06-23 13:56:18 +0900

    2-7 이미지 Preloading

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ function App() {
 
     useEffect(() => {
         LazyImageModal.preload()
+
+        const img = new Image()
+        img.src = 'https://stillmed.olympic.org/media/Photos/2016/08/20/part-1/20-08-2016-Football-Men-01.jpg?interpolation=lanczos-none&resize=*:800'
+        
     }, [])
 
     // const handleMouseEnter = () => {
